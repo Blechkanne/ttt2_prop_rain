58797848ef4a4a7287f68c558328567348153594
Tora <49911006+NovaDiablox@users.noreply.github.com>
2024-12-01 23:44:09 +0300

    Added Turkish L10n

---
+++ lua/terrortown/lang/tr/prop_rain.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("tr")
+
+L["prop_rain_addon_info"] = "Nesne Yağmuru Silahı"
+L["prop_rain_addon_header"] = "Nesne Yağmuru Yapılandırması"
+L["prop_rain_help_menu"] = "Buradan Nesne Yağmuru Silahı'nı istediğiniz şekilde yapılandırabilirsiniz"
+L["label_proprain_despawn_props"] = "Nesnelerin bir süre sonra yok olup olmayacağı (Varsayılan 0)"
+L["label_roprain_despawn_seconds"] = "Kaç saniye sonra yok olacakları (Varsayılan 5)"
+L["label_proprain_sidelength"] = "Etkilenen alanın büyüklüğü (Varsayılan 300)"
+L["label_proprain_proptimer"] = "Nesnelerin ne kadar hızlı oluşturulacağı (ms cinsinden) (Varsayılan 100)"
+L["label_proprain_iterations"] = "Kaç tane nesne oluşturulacağı (Varsayılan 30)"
